23ddb9f906af21272c5876a3dfb713e3e3c2d481
Hazem Elshabini <h.elshabini@outlook.com>
2018-09-20 13:04:39 +0300

    VersionController

---
+++ SampleWebApi/Controllers/VersionController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "V.1.1";
+        }
+    }
+}
